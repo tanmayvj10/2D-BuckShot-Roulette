ab53b1809987e72920ce7ef93444d6f6584e2456
tanmayvj10 <tanmayvj10@gmail.com>
2024-08-14 15:40:28 +0530

    added player image

--- src/player/player.java
+++ src/player/player.java
@@ -11,16 +11,34 @@ public class player {
     public static final int player_x=200 , player_y=180;
     public BufferedImage player_img;
     public player(){
-        
+        getPlayerImage();
     }
+
+    public void getPlayerImage(){
+        try {
+            //player_img=ImageIO.read(getClass().getResourceAsStream("res/player/player.png"));
+            player_img = ImageIO.read(new File("res/player/player.png"));
+            if (player_img == null) {
+                throw new IOException("Image not found.");
+            }
+
+        } catch (IOException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
     public void update(){
+
     }
 
     public void draw(Graphics2D g2){
-        BufferedImage image=player_img;
+        BufferedImage image=null;
+        image=player_img;
+
         g2.drawImage(image,(game_panel.WIDTH-player_x)/2,game_panel.HEIGHT-player_y-margin, player_x, player_y,null);
         // PLAYER
-        g2.fillRect((game_panel.WIDTH-player_x)/2,game_panel.HEIGHT-player_y-margin, player_x, player_y);
+        // g2.fillRect((game_panel.WIDTH-player_x)/2,game_panel.HEIGHT-player_y-margin, player_x, player_y);
     }
 
 }
